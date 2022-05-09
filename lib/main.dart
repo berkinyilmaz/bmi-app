dea8d3abf68a102a22267482a73baabdabc2c1cd
Berkin Yılmaz <62833425+berkinyilmaz@users.noreply.github.com>
2022-05-09 06:13:54 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:bmi_app/constants/app_constants.dart';
+import 'package:flutter/material.dart';
+import 'package:bmi_app/screens/home.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+
+class MyApp extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+    title: "BMI Calculator",
+    debugShowCheckedModeBanner: false,
+    theme: ThemeData(
+      primarySwatch: Colors.yellow,
+      visualDensity: VisualDensity.adaptivePlatformDensity,
+    ),
+    home: HomeScreen(),
+    );
+  }
+}
